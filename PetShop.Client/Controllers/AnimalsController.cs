e6a7ac6a7f96c377b12d1ec5db4f42ccb9d681e9
lital hazot <lital7h@gmail.com>
2022-05-08 11:59:50 +0300

    move the CRUD to admin's responsibility

--- PetShop.Client/Controllers/AnimalsController.cs
+++ PetShop.Client/Controllers/AnimalsController.cs
@@ -47,28 +47,28 @@ namespace PetShop.Client.Controllers
         }
 
         // GET: Animals/Create
-        public IActionResult Create()
-        {
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Name");
-            return View();
-        }
+        //public IActionResult Create()
+        //{
+        //    ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Name");
+        //    return View();
+        //}
 
-        // POST: Animals/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("AnimalId,Name,Description,BirthDate,PhotoUrl,CategoryId")] Animal animal)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(animal);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Name", animal.CategoryId);
-            return View(animal);
-        }
+        //// POST: Animals/Create
+        //// To protect from overposting attacks, enable the specific properties you want to bind to.
+        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("AnimalId,Name,Description,BirthDate,PhotoUrl,CategoryId")] Animal animal)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Add(animal);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Name", animal.CategoryId);
+        //    return View(animal);
+        //}
 
         // GET: Animals/Edit/5
         public async Task<IActionResult> Edit(int? id)
@@ -124,34 +124,34 @@ namespace PetShop.Client.Controllers
         }
 
         // GET: Animals/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var animal = await _context.Animals
-                .Include(a => a.Category)
-                .FirstOrDefaultAsync(m => m.AnimalId == id);
-            if (animal == null)
-            {
-                return NotFound();
-            }
+        //    var animal = await _context.Animals
+        //        .Include(a => a.Category)
+        //        .FirstOrDefaultAsync(m => m.AnimalId == id);
+        //    if (animal == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(animal);
-        }
+        //    return View(animal);
+        //}
 
-        // POST: Animals/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var animal = await _context.Animals.FindAsync(id);
-            _context.Animals.Remove(animal);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+        //// POST: Animals/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    var animal = await _context.Animals.FindAsync(id);
+        //    _context.Animals.Remove(animal);
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
 
         private bool AnimalExists(int id)
         {
